f920c33c54ac9f637a43dd1a5cb9f59adf4269eb
Giselle556 <giselleborbolla@yahoo.com>
2021-09-01 15:10:43 -0700

    comments

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
+// Look to localStorage for any stored activites
+// If there is no item stored under the name of "day-schedule", default to an empty object
+const data = JSON.parse(localStorage.getItem("day-schedule")) || {};
+
+// Find all elements with the class of "time-block"
+$(".time-block").each(function () {
+    // For each time block, execute the following code
+
+    // Retrieve the id from the time block
+    const id = $(this).attr("id");
+
+});
+
+
+
+// Create and format a date object
 var n = new Date();
 var y = n.getFullYear();
 var m = n.getMonth();
 var d = n.getDate();
 document.getElementById("currentDay").innerHTML = m + "/" + d + "/" + y;
 
-var activities = document.querySelector("textarea");
-const data = JSON.parse(localStorage.getItem("day-schedule")) || {};
 
+// Add a click event listener to our saveBtns
+$(".saveBtn").on("click", function(event) {
+    // prevent the default action of our button
+    event.preventDefault();
+    //retrieve the value in our textarea
+    const val = $(this).siblings("textarea").eq(0).val().trim();
+    // retreive the id from our parent element, which would be our time-block
+    const id = $(this).parent().attr("id");
+    // Store the text in our data object
+    data[id] = val;
+    // Store our notes in localStorage so we can retrieve them later.
+    localStorage.setItem("day-schedule", JSON.stringify(data));
+});
